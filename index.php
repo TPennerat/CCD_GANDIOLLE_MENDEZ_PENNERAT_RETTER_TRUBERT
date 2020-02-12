f936ac9e93d01f907f0ddcf8c2e62a141ebbb01d
Théo Pennerat <theo.pennerat1@etu.univ-lorraine.fr>
2020-02-12 16:51:36 +0100

    fonctionnement de base index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*https://webetu.iutnc.univ-lorraine.fr/www/pennerat7u/CCD_GANDIOLLE_MENDEZ_PENNERAT_RETTER_TRUBERT_JENIN/*/
+session_start();
+
+use \Illuminate\Database\Capsule\Manager as DB;
+use Slim\Slim;
+
+require_once('vendor/autoload.php');
+
+$app = new Slim();
+
+$db = new DB();
+$db->addConnection(parse_ini_file('conf.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
+
+//affichage de la racine
+$app->get('/',function () {
+ $c = new ControleurAffichage();
+ $c->racine();
+})->name('racine');
+
+
+$app->run();
