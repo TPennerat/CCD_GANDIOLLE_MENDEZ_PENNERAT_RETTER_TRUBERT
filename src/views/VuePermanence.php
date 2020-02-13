aa9066e016866f242ac43eeee8df7d987e55c417
Valentin Retter <retter4u@etu.univ-lorraine.fr>
2020-02-13 20:50:52 +0100

    modif site internet

--- src/views/VuePermanence.php
+++ src/views/VuePermanence.php
@@ -103,7 +103,7 @@ END;
         $creerCompte=$app->urlFor('creerCompte');
         $modifCompte=$app->urlFor('modifierCompte');
         $graphique=$app->urlFor('graphique');
-        $users = $app->urlFor('afficherUsers');
+        $users = $app->urlFor('users');
         $res = <<<END
       <!DOCTYPE html>
       <html lang="en">
