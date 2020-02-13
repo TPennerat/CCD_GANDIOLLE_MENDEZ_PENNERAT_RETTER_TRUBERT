0a126158d0a29588ed9510ba146ee3fbbb4a2f35
Tom Mendez-Porcel <mendezpo1u@etu.univ-lorraine.fr>
2020-02-13 11:31:10 +0100

    ajout de la vue et du controleur permanence, ébauche des méthodes de création de besoin

---
+++ src/controlers/ControleurPermanence.php
@@ -0,0 +1,19 @@
+<?php
+namespace epicerie\controlers;
+
+use \epicerie\views\VueComptes as VueComptes;
+use \epicerie\views\VueAccueil as VueAccueil;
+use \epicerie\models\Authentification as Authentification;
+use \epicerie\models\User as User;
+use \Exception;
+use Slim\Slim;
+
+
+class ControleurPermanence {
+    //Affiche la page de création d'un besoin
+    function afficherCreationBesoin() {
+
+        $view = new VuePermanence();
+        $view->render(1);
+    }
+}
---
+++ src/views/VuePermanence.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace epicerie\views;
+
+class VuePermanence {
+
+    public $arr;
+
+    public function __construct($a)
+    {
+        $this->arr = $a;
+    }
+
+    private function afficherCréationBesoin(){
+        $html="";
+
+        //FORMULAIRE
+        $html .="<form method=\"post\">";
+        $html .= '<input type="text" name="role" required placeholder="Role">';
+        $html .= '<input type="text" name="creneau" required placeholder="Créneau">';
+        $html .= '<button type=submit name="valider">Envoyer</button>';
+
+        $html.="</form>";
+
+
+        return $html;
+    }
+
+    public function render($selecteur)
+    {
+        $app = Slim::getInstance();
+        $content = "";
+        switch ($selecteur) {
+            case 1:
+            {
+                $content = $this->afficherCréationBesoin();
+                break;
+            }
+            $html = "contenu à créer (Marius/valentin) + ".$content." + contenu à créer (Marius/valentin)";
+            return content;
+
+        }
+    }
+
+}
