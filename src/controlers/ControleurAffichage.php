179406a8197e21237bef3162ac1c77d46e505164
Gandiolle Eliott <eliott.gandiolle1@etu.univ-lorraine.fr>
2020-02-13 12:48:17 +0100

    VuePermanences

--- src/controlers/ControleurAffichage.php
+++ src/controlers/ControleurAffichage.php
@@ -11,8 +11,8 @@ class ControleurAffichage
 {
     public function racine() {
         if (isset($_SESSION['id_connect'])) {
-            echo "cc";
-            var_dump($_SESSION['id_connect']);
+            $cont = new ControleurAffichage();
+            $cont->afficherMesPermanences(); 
         } else {
             Slim::getInstance()->redirect(Slim::getInstance()->urlFor('connexion'));
         }
