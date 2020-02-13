7e98726016be031ad67c9db2d0fa85fe589f7e3c
Gandiolle Eliott <eliott.gandiolle1@etu.univ-lorraine.fr>
2020-02-13 13:21:39 +0100

    controleur permanence

--- src/controlers/ControleurPermanence.php
+++ src/controlers/ControleurPermanence.php
@@ -9,16 +9,24 @@ use Slim\Slim;
 
 
 class ControleurPermanence {
-    //Affiche la page de création d'un besoin
-    function afficherCreationBesoin() {
+  //Affiche la page de création d'un besoin
+  function afficherCreationBesoin() {
 
-        $view = new VuePermanence();
-        $view->render(1);
-    }
+    $view = new VuePermanence();
+    $view->render(1);
+  }
 
-    function afficherMesPermanences($id) {
-        $perms = Permanence::where('idUtil','=',$id)->get();
-        $view = new VuePermanence($perms);
-        $view->render(2);
-    }
+  function creerBesoin() {
+
+     $view = new VuePermanence();
+     
+
+  }
+
+
+  function afficherMesPermanences($id) {
+    $perms = Permanence::where('idUtil','=',$id)->get();
+    $view = new VuePermanence($perms);
+    $view->render(2);
+  }
 }
