c14e18f07abd3c9dcbb931b90acf162f7314000b
Gandiolle Eliott <eliott.gandiolle1@etu.univ-lorraine.fr>
2020-02-13 10:24:14 +0100

    ajout

---
+++ src/models/User.php
@@ -0,0 +1,18 @@
+<?php
+namespace epicerie\models;
+
+class User extends \Illuminate\Database\Eloquent\Model {
+  protected $table = 'user';
+  protected $primaryKey = 'idu';
+
+  public $timestamps = false;
+
+  public function listes(){
+    return $this->hasMany('Liste','idu');
+  }
+
+  public function participations() {
+    return $this->hasMany('Participation','idu');
+  }
+
+}
